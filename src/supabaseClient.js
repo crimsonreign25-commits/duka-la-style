e5c00cd075a1c0640696d01c0f10ca752ee4a2b4
crimsonreign25-commits <crimsonreign25@gmail.com>
2026-08-20 00:55:37 +0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,9 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const supabaseUrl = "https://uzkdlyyjnxagflsnsuxo.supabase.co";
+
+const supabaseAnonKey =
+  "sb_publishable_un3LFqiZKyMi8bShcAQZmg_BD_nqBE3";
 
 export const supabase = createClient(
   supabaseUrl,
-  supabaseKey
+  supabaseAnonKey
 );
